2e1fe113b6d2c404bb4baf4073ed1110d0dfd3f7
kirill zharov <pcmarket2020@gmail.com>
2024-05-16 21:01:54 +0300

    HW_4 done

--- homework/kirill_zharov/HW_4/task.py
+++ homework/kirill_zharov/HW_4/task.py
@@ -3,10 +3,10 @@ my_dict = {
     "tuple": (1, "one", 2, "two", 5),
     "list": [1, 2, 3, 4, 5],
     "dict": {"one": "apple", "fruit": "cherкy", "animals": "bear"},
-     "set": {2, None, 4.5, False, 8}
-     }
+    "set": {2, None, 4.5, False, 8}
+}
 
-#last_element1 = my_dict.get('two', 5)
+# last_element1 = my_dict.get('two', 5)
 last_element2 = my_dict["tuple"][-1]
 
 my_dict["list"].append(6)
