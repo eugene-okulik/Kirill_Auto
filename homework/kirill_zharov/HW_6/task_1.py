6d12af5fee078a2abb61a6d33a19eba98560827e
kirill zharov <pcmarket2020@gmail.com>
2024-05-21 22:10:34 +0300

    HW_6_done

--- homework/kirill_zharov/HW_6/task_1.py
+++ homework/kirill_zharov/HW_6/task_1.py
@@ -1,4 +1,5 @@
-some_text = "Etiam tincidunt neque erat, quis molestie enim imperdiet vel. Integer urna nisl, facilisis vitae semper at, dignissim vitae libero."
+some_text = "Etiam tincidunt neque erat, quis molestie enim imperdiet vel. Integer urna nisl,\
+ facilisis vitae semper at, dignissim vitae libero."
 
 punctuation_marks = [":", ".", "!", "?", ",", ";"]
 
