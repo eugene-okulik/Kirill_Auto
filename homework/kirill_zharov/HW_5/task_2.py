5059a48f0137b007e648f449d22cd7fbcbaa37b8
kirill zharov <pcmarket2020@gmail.com>
2024-05-21 13:04:01 +0300

    HW_5 task_2 solution using slices and index method

--- homework/kirill_zharov/HW_5/task_2.py
+++ homework/kirill_zharov/HW_5/task_2.py
@@ -11,8 +11,8 @@ number_1 = int(str_1[colon_index_1 + 2:]) + 10
 number_2 = int(str_2[colon_index_2 + 2:]) + 10
 number_3 = int(str_3[colon_index_3 + 2:]) + 10
 
-#print(number_1)
-#print(number_2)
-#print(number_3)
+# print(number_1)
+# print(number_2)
+# print(number_3)
 
 print(f"Результат сложения: {number_1} {number_2} {number_3}")
