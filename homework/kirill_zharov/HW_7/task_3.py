7b568efe6e6ee89b61bdcf5564e9933a95ef1d78
kirill zharov <pcmarket2020@gmail.com>
2024-05-23 16:07:44 +0300

    HW_7_done

---
+++ homework/kirill_zharov/HW_7/task_3.py
@@ -0,0 +1,13 @@
+def some_func(results):
+    for text in results:
+        number = int(text.split()[-1]) + 10
+        print(number)
+
+results = [
+    "результат операции: 42",
+    "результат операции: 54",
+    "результат работы программы: 209",
+    "результат: 2"
+]
+
+some_func(results)
