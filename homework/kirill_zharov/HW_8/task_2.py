2e7a01a0a1d728994e0a54ea049fa04f5422ce85
kirill zharov <pcmarket2020@gmail.com>
2024-05-27 10:20:07 +0300

    HW_8_done

--- homework/kirill_zharov/HW_8/task_2.py
+++ homework/kirill_zharov/HW_8/task_2.py
@@ -1,15 +1,17 @@
 import sys
 
-# Увеличение лимита на количество символов в строковом представлении целых чисел
+# Устанавливаем лимит на количество символов в строковом представлении целых чисел
 sys.set_int_max_str_digits(100000)
 
 def fibonacci_generator():
+    """Генератор чисел Фибоначчи."""
     a, b = 0, 1
     while True:
         yield a
         a, b = b, a + b
 
 def get_fibonacci_number_at_index(index):
+    """Возвращает число Фибоначчи по заданному индексу."""
     fib_gen = fibonacci_generator()
     for _ in range(index):
         number = next(fib_gen)
