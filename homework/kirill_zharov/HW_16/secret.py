3713f4c51cad96a84038e0e371c0c5fdec017c9d
kirill zharov <pcmarket2020@gmail.com>
2024-07-17 16:39:35 +0300

    HW_16_done

--- homework/kirill_zharov/HW_16/secret.py
+++ homework/kirill_zharov/HW_16/secret.py
@@ -1,10 +1,11 @@
-import mysql.connector as mysql
-import cred
 import os
-import dotenv
 import csv
+import dotenv
+import mysql.connector as mysql
 
+# Загрузка переменных окружения из .env файла
 dotenv.load_dotenv()
+
 # Подключение к базе данных
 db = mysql.connect(
     user=os.getenv('DB_USER'),
@@ -14,20 +15,19 @@ db = mysql.connect(
     database=os.getenv('DB_NAME')
 )
 
+# Получаем базовый путь к директории, в которой находится текущий файл скрипта
 base_path = os.path.dirname(__file__)
 homework_path = os.path.dirname(os.path.dirname(base_path))
-# print(homework_path)
+
 # Построение пути к файлу конфигурации относительно текущего файла
 config_path = os.path.join(homework_path, "eugene_okulik", "Lesson_16", "hw_data", "data.csv")
-#print(config_path)
 
+# Чтение данных из CSV файла
 with open(config_path, newline='') as csvfile:
     csv_reader = csv.reader(csvfile)
     csv_data = [row for row in csv_reader]
 
 csv_data_tuples = [tuple(row) for row in csv_data]
-
-#print(csv_data_tuples)
 
 # Создание объекта для выполнения SQL-запросов
 cursor = db.cursor(dictionary=True)
@@ -36,9 +36,9 @@ SELECT *
 FROM students;
 """)
 data = cursor.fetchall()
-db_data_tuples = [tuple(row) for row in data]
-#print(db_data_tuples)
+db_data_tuples = [tuple(row.values()) for row in data]
 
+# Поиск отсутствующих данных
 missing_data = [row for row in csv_data_tuples if row not in db_data_tuples]
 
 if missing_data:
@@ -51,4 +51,3 @@ else:
 # Закрытие соединения с базой данных
 cursor.close()
 db.close()
-
